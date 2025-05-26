bf710fe7698d469925403f02ca0a8df40596c3cb
John Alwin James Puracan <johnalwinjamespuracan@gmail.com>
2025-05-26 21:59:35 +0800

    Entry Page (Login Page)
    Starting point of the system, the Login/Entry Page

---
+++ view/EntryPage.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>SarisMart</title>
+
+	<!--JQUERY UI & JS -->
+	<link rel="stylesheet" href="../assets/jquery/base/jquery-ui.css">
+	<script src="../assets/jquery/jquery.js"></script>
+	<script src="../assets/jquery/jquery-ui.js"></script>
+
+	<!-- Bootstrap 5 -->
+	<link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+	<script src="../assets/js/bootstrap.bundle.min.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="../assets/style.css">
+
+</head>
+
+<body>
+	<div class="container body-container">
+		<div class="row">
+			<div class="col d-flex justify-content-center ">
+				<div class="content-form text-center">
+					<form class="form-login" method="POST">
+						<h1 class="entryH">SARISMART</h1>
+						<p class="entryP">Enter Password to Proceed</p>
+						<input class="entryfield" type="text" name="passfield" placeholder="Password">
+						<div class="mt-3 text-center">
+							<button type="submit" class="btn btn-entry">Login</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+
+</html>
