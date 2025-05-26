aa14e30a82b90ce5a249ef639ae86c7d7d2ca7f1
Rhazeanneth Ajoc <142295302+R-Ajoc@users.noreply.github.com>
2025-05-26 23:42:27 +0800

    Update EntryPage.php

--- view/EntryPage.php
+++ view/EntryPage.php
@@ -1,40 +1,29 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>SarisMart</title>
-
-	<!--JQUERY UI & JS -->
-	<link rel="stylesheet" href="../assets/jquery/base/jquery-ui.css">
-	<script src="../assets/jquery/jquery.js"></script>
-	<script src="../assets/jquery/jquery-ui.js"></script>
-
-	<!-- Bootstrap 5 -->
-	<link href="../assets/css/bootstrap.min.css" rel="stylesheet">
-	<script src="../assets/js/bootstrap.bundle.min.js"></script>
-
-	<link rel="stylesheet" type="text/css" href="../assets/style.css">
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SARISMART Login</title>
+    <link href="../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/style.css" rel="stylesheet">
 </head>
-
 <body>
-	<div class="container body-container">
-		<div class="row">
-			<div class="col d-flex justify-content-center ">
-				<div class="content-form text-center">
-					<form class="form-login" method="POST">
-						<h1 class="entryH">SARISMART</h1>
-						<p class="entryP">Enter Password to Proceed</p>
-						<input class="entryfield" type="text" name="passfield" placeholder="Password">
-						<div class="mt-3 text-center">
-							<button type="submit" class="btn btn-entry">Login</button>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
-</body>
+    <div class="login-wrapper">
+        <h1 class="login-title">SARISMART</h1>
+        <p class="login-subtitle">Enter password to proceed</p>
 
+        <?php if (isset($_GET['error'])): ?>
+            <div class="alert alert-danger py-1">Invalid password. Please try again.</div>
+        <?php endif; ?>
+
+        <form class="login-form" action="../controller/adminsecurityController.php?action=login" method="POST">
+            <input type="password" name="password" class="form-control mb-2" placeholder="password" required>
+            <div class="d-flex justify-content-center">
+                <button type="submit" class="btn btn-primary btn-login">Login</button>
+            </div>
+        </form>
+    </div>
+
+    <script src="../assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
 </html>
